c46132a4cdda7d26d9d2bc74ca97adaca2af1f17
TurtleSnakeMoose <misharoskin@gmail.com>
2019-08-28 22:46:34 +0300

    added

---
+++ script.js
@@ -0,0 +1,105 @@
+$(function (){
+	
+	// start button : correct number if num > 25 OR num < 2. display attendant names and payment inputs.
+	$('.btn_start').on('click', function(e) {
+		var numOfAttendants =  parseInt($('#input_attendNum').val());
+		appendAttendantInfo(numOfAttendants < 2 ? 2 : numOfAttendants > 25 ? 25 : numOfAttendants)
+	});
+
+	// append attendant names and payment inputs to form 
+	function appendAttendantInfo(numOfAttendants){
+		var payersDiv = $('#panel_payers');
+		$(payersDiv).empty();
+
+		for(i = 0 ; i < numOfAttendants ; i++){
+			payersDiv.append(`
+			<div class="form-group row attendantRow">
+				
+				<label class="col-1 col-form-label">${i+1}. </label>
+
+				<div class="col-5">
+					<input class="form-control attendantName" type="text" placeholder="Attendant#${i+1}" id="attndName${i+1}" data-attendantNum="${i+1}">
+				</div>
+
+				<label class="col-1 col-form-label">Paid </label>
+
+				<div class="col-1" style="padding: 0 1vw">
+					<input class="form-control attendantPaid" type="number" placeholder="0" id="attndPayment_${i+1}" style="width:5vw; data-attendantNum="${i+1}"">
+				</div>
+
+			</div>	
+			`);	
+		}
+
+		$(payersDiv).append(`
+			<div style="text-align: center;">
+				<button class="btn btn-primary btn_calculate">Calculate</button>
+			</div>
+		`);
+
+		// calculate button: calculate for every memeber of the party, how much money he should transfer to who
+		$('.btn_calculate').on('click', function(e) {
+			calcPayments();
+		});
+	}
+
+	// calculate the funds transaction
+	function calcPayments (){
+		var payersPanel = $('#panel_payers'),
+			attndCount = parseInt($('#input_attendNum').val()),
+			attndData = [],
+			sum = 0;
+			precalculatedEachShare = 0;
+
+			payersPanel.find('.attendantRow').each(function (i, el){
+				var attendant = $(el),
+					atndName = attendant.find('.attendantName').val(),
+					atndPaid = parseInt(attendant.find('.attendantPaid').val() === '' ? 0 : attendant.find('.attendantPaid').val());
+				sum += atndPaid;
+				attndData.push({Name: atndName, Paid: atndPaid});
+			});
+
+			precalculatedEachShare = sum / attndCount;
+			var resultContent = buildResultContent(attndCount, sum, precalculatedEachShare, attndData)
+			displayCalculation(resultContent);
+	}
+	
+	// build the result string based on input data
+	function buildResultContent(attndCount, sum, precalculatedEachShare, attndData){
+		console.table(attndData);
+
+		var resultPanel = $('#panel_result'),
+			payersPanel = $('#panel_payers');
+		
+		//CONTINUE HERE
+		var attendantBalance = $(attndData).clone(true);
+		debugger;
+
+		var resultContent = `
+			<span>Combined sum of <strong>${sum}</strong> was collected by all <strong>${attndCount} attendants</strong>.</span><br>
+			<span>Split evenly between all attendies BEFORE calculated distribution is <strong>${precalculatedEachShare}</strong>.</span>
+		`;
+
+		return resultContent;
+
+	}
+	
+	// switch between panels to display the html content
+	function displayCalculation(content){
+
+		var resultPanel = $('#panel_result'),
+			payersPanel = $('#panel_payers'),
+			buttonContent = `<div style="text-align: center;"><button class="btn btn-primary btn_back">Back</button></div>`;
+
+		resultPanel.show().empty();
+		payersPanel.hide();
+
+		resultPanel.append(content + buttonContent);
+
+		$('.btn_back').on('click', function(){
+			resultPanel.hide();
+			payersPanel.show();
+		})
+	}
+
+})
